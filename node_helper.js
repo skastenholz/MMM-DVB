a86e5109c5f03a7743fad11d04748efcb5ce28f9
skastenholz <stefan.kastenholz@gmail.com>
2019-08-13 23:04:38 +0200

    Update dvbjs dependency

--- node_helper.js
+++ node_helper.js
@@ -3,17 +3,21 @@ var NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
 
-    socketNotificationReceived: function(notification, payload) {
+	socketNotificationReceived: function(notification, payload) {
         if (notification === 'DVB-REQUEST') {
-            var self = this;
-            dvb.monitor(payload.stopName, payload.timeOffset, this.numberOfRequestedResults(payload), function(err, data) {
-                if (err) throw err;
-                var response = {
-                    id: payload.id,
-                    connections: self.connectionsToBeDisplayed(data, payload)
-                };
-                self.sendSocketNotification("DVB-RESPONSE", response);
-            });
+            var self = this
+			dvb.findStop(payload.stopName).then((data) => {
+				if (Array.isArray(data) && data.length > 0) {
+					var stopID = data[0].id
+					dvb.monitor(stopID, payload.timeOffset, this.numberOfRequestedResults(payload)).then((data) => {
+						var response = {
+							id: payload.id,
+							connections: self.connectionsToBeDisplayed(data, payload)
+						};
+						self.sendSocketNotification("DVB-RESPONSE", response);
+					});
+				}
+			});
         }
     },
     numberOfRequestedResults: function(payload) {
